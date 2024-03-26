a94ef16664caf8201b9102b385df405592e36542
Draxx0 <williamfort.lmgl@gmail.com>
2024-03-26 15:13:30 +0100

    :sparkles: add sanititaries filter

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import { Alert, Linking, StyleSheet, View } from 'react-native';
+import { Alert, Linking, StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { MAPCONSTANTS } from '~/constants/map.constants';
 import * as Location from 'expo-location';
-import useGetSanitaries from '~/hooks/useGetSanitaries';
+import SANITARIES from '~/data/sanitaries.json';
 
 export default function Map() {
   const [location, setLocation] = useState<Location.LocationObject | null>(
@@ -11,8 +11,9 @@ export default function Map() {
   );
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const [selectedMarker, setSelectedMarker] = useState<any>(null);
-
-  const { data, isLoading } = useGetSanitaries();
+  const [filteredSanitaries, setFilteredSanitaries] = useState<
+    any | Array<any>
+  >(null);
 
   useEffect(() => {
     (async () => {
@@ -37,6 +38,28 @@ export default function Map() {
     })();
   }, []);
 
+  const handleGetNearestSanitaries = ({
+    lat,
+    lon,
+  }: {
+    lat: number;
+    lon: number;
+  }) => {
+    const sortedSanitaries = SANITARIES.sort((a: any, b: any) => {
+      const distanceA = Math.sqrt(
+        Math.pow(a.geo_point_2d.lat - lat, 2) +
+          Math.pow(a.geo_point_2d.lon - lon, 2)
+      );
+      const distanceB = Math.sqrt(
+        Math.pow(b.geo_point_2d.lat - lat, 2) +
+          Math.pow(b.geo_point_2d.lon - lon, 2)
+      );
+      return distanceA - distanceB;
+    });
+    console.log('sortedSanitaries', sortedSanitaries.slice(0, 10));
+    setFilteredSanitaries(sortedSanitaries.slice(0, 10));
+  };
+
   const handleMarkerPress = (marker: any) => {
     setSelectedMarker(marker);
     if (location && marker) {
@@ -44,7 +67,11 @@ export default function Map() {
         `Itinéraire - ${marker.adresse}`,
         `Latitude: ${marker.geo_point_2d.lat}, Longitude: ${marker.geo_point_2d.lon}`,
         [
-          { text: 'Annuler', style: 'cancel' },
+          {
+            text: 'Annuler',
+            style: 'cancel',
+            onPress: () => setSelectedMarker(null),
+          },
           {
             text: 'Ouvrir Maps',
             onPress: () => {
@@ -57,60 +84,73 @@ export default function Map() {
     }
   };
 
-  return (
-    // INSERT BLUR ON MAP AND DISABLED MAP IF LOCATION IS NOT ENABLED
+  useEffect(() => {
+    console.log('filteredSanitaries', filteredSanitaries, 'location', location);
+  }, [filteredSanitaries]);
 
-    location ? (
+  return location ? (
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={styles.map}
+      userLocationPriority="high"
+      accessibilityElementsHidden={true}
+      followsUserLocation={true}
+      showsUserLocation={true}
+      showsMyLocationButton={true}
+      loadingEnabled={true}
+      showsCompass={true}
+      onRegionChangeComplete={(region, details) => {
+        handleGetNearestSanitaries({
+          lat: region.latitude,
+          lon: region.longitude,
+        });
+      }}
+      initialRegion={
+        location
+          ? {
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }
+          : {
+              ...MAPCONSTANTS.initialRegion,
+            }
+      }
+    >
+      {filteredSanitaries &&
+        filteredSanitaries.length > 0 && // Correction de la faute de frappe ici
+        filteredSanitaries.map((sanitary: any, index: number) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: sanitary.geo_point_2d.lat,
+              longitude: sanitary.geo_point_2d.lon,
+            }}
+            icon={
+              selectedMarker === sanitary
+                ? require('~/assets/images/selected-marker.png')
+                : require('~/assets/images/marker.png')
+            }
+            onPress={() => handleMarkerPress(sanitary)}
+          />
+        ))}
+    </MapView>
+  ) : (
+    // DISABLED MAP
+    <View style={{ flex: 1 }}>
       <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        userLocationPriority="high"
-        accessibilityElementsHidden={true}
-        followsUserLocation={true}
-        showsUserLocation={true}
-        showsMyLocationButton={true}
-        loadingEnabled={!!isLoading}
-        initialRegion={
-          location
-            ? {
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-              }
-            : {
-                ...MAPCONSTANTS.initialRegion,
-              }
-        }
-      >
-        {data &&
-          data.map((sanitary: any, index: number) => (
-            <Marker
-              key={index}
-              coordinate={{
-                latitude: sanitary.geo_point_2d.lat,
-                longitude: sanitary.geo_point_2d.lon,
-              }}
-              onPress={() => handleMarkerPress(sanitary)}
-            />
-          ))}
-      </MapView>
-    ) : (
-      // DISABLED MAP
-      <View style={{ flex: 1 }}>
-        <MapView
-          style={styles.mapDisabled}
-          initialRegion={{
-            ...MAPCONSTANTS.initialRegion,
-          }}
-          zoomEnabled={false}
-          scrollEnabled={false}
-          pitchEnabled={false}
-          rotateEnabled={false}
-          zoomTapEnabled={false}
-        />
-      </View>
-    )
+        style={styles.mapDisabled}
+        initialRegion={{
+          ...MAPCONSTANTS.initialRegion,
+        }}
+        zoomEnabled={false}
+        scrollEnabled={false}
+        pitchEnabled={false}
+        rotateEnabled={false}
+        zoomTapEnabled={false}
+      />
+    </View>
   );
 }
 
